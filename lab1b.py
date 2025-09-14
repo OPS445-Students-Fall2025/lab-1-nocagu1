a26456d30e3089d70b6f43f2b1dd43e7f5c0c66e
Nnamdi Agu <nagu1@myseneca.ca>
2025-09-14 17:31:11 -0400

    Added lab1b

---
+++ lab1b.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+#!/usr/bin/env python3
+
+# lab1b.py
+
+# Create a variable called 'name' and assign it the value 'Isaac'
+name = "Isaac"
+
+# Print the question using the name variable
+print(f"How old are you {name}?")
+
