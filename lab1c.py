abe1dc4388261f6fda8da89438180fc63f9eb865
Nnamdi Agu <nagu1@myseneca.ca>
2025-09-15 00:59:58 -0400

    Added lab1c

---
+++ lab1c.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+#Creating an object called name
+#Creating object called age
+#Creating object age to equal 72
+
+name='Isaac'
+age=72
+
+
+
+#Print Isaac is 72 years old!
+
+
+
+print (name + " is " + str(age) + " years old!")
