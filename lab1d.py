a4fcdbb1040ac80e4cc88a228c64fe170aad0aa4
Nnamdi Agu <nagu1@myseneca.ca>
2025-09-17 14:30:15 -0400

    Added Lab1d

---
+++ lab1d.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+#The object x should contain a integer with the value 10
+x=10
+
+#The object y should contain a integer with the value 2
+y=2
+
+#The object z should contain a integer with the value 5
+z=5
+
+#Sum
+result=20
+
+#Sum of each interger combined
+print(str(x) + " + " + str(y) + " * " + str(z) + " = " + str(result))
+
+
+
